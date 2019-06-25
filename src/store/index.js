af14554eee44d840c6492ad9ab113ca0e0ef8826
liubich <liubich.a@gmail.com>
2019-06-25 21:02:38 +0300

    Eslint + prettier

--- src/store/index.js
+++ src/store/index.js
@@ -1,16 +1,16 @@
-import Vue from "vue";
-import Vuex from "vuex";
-import * as mutationTypes from "./mutationTypes";
-import * as utils from "./utils";
+import Vue from 'vue';
+import Vuex from 'vuex';
+import * as mutationTypes from './mutationTypes';
+import * as utils from './utils';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    APIkey: "a68923448fdd58dfc8e56fc58e5c3aa7",
+    APIkey: 'a68923448fdd58dfc8e56fc58e5c3aa7',
     currentPosition: {
       latitude: null,
-      longitude: null
+      longitude: null,
     },
     currentWeather: {
       icon: null,
@@ -22,9 +22,9 @@ export default new Vuex.Store({
       windSpeed: null,
       windDirection: null,
       windDirectionDeg: null,
-      pressure: null
+      pressure: null,
     },
-    errorDesc: null
+    errorDesc: null,
   },
   mutations: {
     [mutationTypes.SAVE_POSITION](state, currentPosition) {
@@ -35,29 +35,29 @@ export default new Vuex.Store({
     },
     [mutationTypes.SAVE_ERROR_DESC](state, errorDesc) {
       state.errorDesc = errorDesc;
-    }
+    },
   },
   getters: {
-    isWeatherGot: state => !!state.currentWeather.description
+    isWeatherGot: state => !!state.currentWeather.description,
   },
   actions: {
     getCurrentPositionAndWeather({ commit, dispatch }) {
-      const onSuccess = pos => {
+      const onSuccess = (pos) => {
         commit(mutationTypes.SAVE_POSITION, pos.coords);
-        dispatch("getCurrentWeather");
+        dispatch('getCurrentWeather');
       };
 
-      const onError = error => {
+      const onError = (error) => {
         commit(mutationTypes.SAVE_ERROR_DESC, utils.getReadableErrorDesc(error));
       };
 
       const options = {
         enableHighAccuracy: false,
         timeout: 30000,
-        maximumAge: 0
+        maximumAge: 0,
       };
       if (!navigator.geolocation) {
-        commit(mutationTypes.SAVE_ERROR_DESC, "Ваш браузер не підтримує геолокацію");
+        commit(mutationTypes.SAVE_ERROR_DESC, 'Ваш браузер не підтримує геолокацію');
         return;
       }
       navigator.geolocation.getCurrentPosition(onSuccess, onError, options);
@@ -67,18 +67,17 @@ export default new Vuex.Store({
       const currentWeatherUrl = utils.getCurrentWeatherAPIUrl({
         latitude: state.currentPosition.latitude,
         longitude: state.currentPosition.longitude,
-        APIkey: state.APIkey
+        APIkey: state.APIkey,
       });
       fetch(currentWeatherUrl)
-        .then(response => {
+        .then((response) => {
           if (response.ok) return response.json();
           commit(mutationTypes.SAVE_ERROR_DESC, response.statusText);
           throw new Error(`HTTP error, status = ${response.status}`);
         })
-        .then(json => {
+        .then((json) => {
           commit(mutationTypes.SAVE_WEATHER, utils.translateJSONToCurrentWeather(json.currently));
-          return;
         });
-    }
-  }
+    },
+  },
 });

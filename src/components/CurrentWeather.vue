c9a7bfcf50f76f7aa65ea659e3c77f9247ca6924
liubich <liubich.a@gmail.com>
2019-06-30 00:01:19 +0300

    Row design

--- src/components/CurrentWeather.vue
+++ src/components/CurrentWeather.vue
@@ -1,37 +1,37 @@
 <template functional>
   <div class="current-weather">
-    <p class="current-weather__datetime">Останнє оновлення: {{props.weather.dateTime}}</p>
-    <h2 class="current-weather__header">{{props.currentPosition.city}}</h2>
-    <div class="current-weather__columns-container">
-      <div class="current-weather__left-column column">
-        <h2 class="current-weather__temperature">
-          <span class="current-weather__temperature-digit">{{props.weather.temperature}}</span>
-          °C
-        </h2>
-        <p class="current-weather__description">{{props.weather.description}}</p>
-      </div>
-      <div class="current-weather__middle-column column">
-        <img alt="weather icon" :src="props.weather.icon">
-      </div>
-      <div class="current-weather__right-column column">
-        <div class="current-weather__wind-container">
-          <div class="current-weather__wind-caption">вітер:</div>
-          <div
-            v-if="props.weather.windSpeed"
-            :style="{transform: `rotate(${props.weather.windDirectionDeg}deg)`}"
-            class="current-weather__wind-direction"
-          >&#8681;</div>
-          <div v-if="props.weather.windSpeed" class="current-weather__wind-caption">
-            {{props.weather.windDirection}} ,
-            {{props.weather.windSpeed}} м/с
-          </div>
-          <div v-else class="current-weather__wind-caption">штиль</div>
+    <h2 class="current-weather__city">{{props.currentPosition.city}}</h2>
+    <div class="current-weather__row">
+      <h2 class="current-weather__temperature">
+        <span class="current-weather__temperature-digit">{{props.weather.temperature}}</span>
+        °C
+      </h2>
+      <img alt="weather icon" :src="props.weather.icon">
+    </div>
+    <div class="current-weather__row">
+      <p class="current-weather__description">{{props.weather.description}}</p>
+    </div>
+    <div class="current-weather__row">
+      <p class="current-weather__pressure">
+        тиск:
+        {{props.weather.pressure}} гПа, {{props.weather.pressureTendency}}
+      </p>
+      <div class="current-weather__wind-container">
+        <div class="current-weather__wind-caption">вітер:</div>
+        <div
+          v-if="props.weather.windSpeed"
+          :style="{transform: `rotate(${props.weather.windDirectionDeg}deg)`}"
+          class="current-weather__wind-direction"
+        >&#8681;</div>
+        <div v-if="props.weather.windSpeed" class="current-weather__wind-caption">
+          {{props.weather.windDirection}} ,
+          {{props.weather.windSpeed}} м/с
         </div>
-        <p class="current-weather__pressure">
-          тиск:
-          {{props.weather.pressure}} гПа, {{props.weather.pressureTendency}}
-        </p>
+        <div v-else class="current-weather__wind-caption">штиль</div>
       </div>
+    </div>
+    <div class="current-weather__row">
+      <p class="current-weather__datetime">Останнє оновлення: {{props.weather.dateTime}}</p>
     </div>
   </div>
 </template>
@@ -45,13 +45,21 @@ export default {
 
 <style scoped>
 .current-weather {
+  min-width: 400px;
   max-width: 540px;
-  margin: 0 auto;
   padding: 5px;
   display: flex;
   align-items: flex-start;
   flex-direction: column;
   border: thick double #32a1ce;
+}
+
+.current-weather__row {
+  width: 100%;
+  padding: 0 10px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 }
 
 .current-weather__datetime {
@@ -60,51 +68,23 @@ export default {
   font-size: 12px;
 }
 
-.current-weather__header {
+.current-weather__city {
   padding-top: 4px;
-  align-self: center;
   font-family: Oswald, sans-serif;
-  font-weight: 600;
-  font-size: 24px;
-  text-transform: uppercase;
-}
-
-.current-weather__columns-container {
-  display: flex;
-  flex-wrap: wrap;
-}
-
-.column {
-  display: flex;
-  flex-direction: column;
-  padding: 5px;
-}
-
-.current-weather__left-column,
-.current-weather__middle-column {
-  align-items: center;
-}
-
-.current-weather__right-column,
-.current-weather__left-column {
-  justify-content: flex-end;
-}
-
-.current-weather__img-container {
-  display: flex;
+  font-size: 16px;
 }
 
 .current-weather__temperature {
   padding-left: 4px;
   font-family: Oswald, sans-serif;
   font-weight: 600;
-  font-size: 30px;
+  font-size: 50px;
 }
 
 .current-weather__temperature-digit {
   font-family: Oswald, sans-serif;
   font-weight: 600;
-  font-size: 50px;
+  font-size: 90px;
 }
 
 .current-weather__pressure {

8775f50441cd75b5492e016dc43e893dbe8780e7
liubich <liubich.a@gmail.com>
2019-08-27 22:19:06 +0300

    Remove useless getFormattedDateStr function

--- src/store/utils.js
+++ src/store/utils.js
@@ -7,17 +7,6 @@ export const getReadableErrorDesc = (error) => {
   return errorCodeToDescription[error.code] || 'Невідома помилка';
 };
 
-const getFormattedDateStr = (UNIXdate) => {
-  const reqDate = new Date(parseInt(UNIXdate, 10));
-  return reqDate.toLocaleString('uk-UA', {
-    weekday: 'long',
-    month: 'long',
-    day: 'numeric',
-    hour: 'numeric',
-    minute: 'numeric',
-  });
-};
-
 const getActualIconNumber = (iconNumber) => {
   const iconMapping = {
     3: '2',

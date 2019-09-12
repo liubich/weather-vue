c47aea8be87f63d844873d0eb53fc8a1bd3b2f91
E\LiubichOM <Oleksandr.Liubich@e-consulting.com.ua>
2019-09-12 17:09:03 +0300

    Exit from plugin if browser doesn't use local storage

--- src/store/localStoragePlugin.js
+++ src/store/localStoragePlugin.js
@@ -2,20 +2,19 @@ import * as mutationTypes from './mutationTypes';
 import * as utils from './utils';
 
 export default function localStoragePlugin(store) {
-  if (localStorage) {
-    const lastKnownPositionFromLocalStorage = localStorage.getItem('lastKnownPosition');
-    const currentWeatherFromLocalStorage = localStorage.getItem('currentWeather');
-    if (
-      lastKnownPositionFromLocalStorage
-      && currentWeatherFromLocalStorage
-      && !store.state.currentWeather.dataLoadedFromAPI
-    ) {
-      store.commit(
-        mutationTypes.SAVE_CURRENT_POSITION_DATA,
-        JSON.parse(lastKnownPositionFromLocalStorage),
-      );
-      store.commit(mutationTypes.SAVE_WEATHER, JSON.parse(currentWeatherFromLocalStorage));
-    }
+  if (!localStorage) return;
+  const lastKnownPositionFromLocalStorage = localStorage.getItem('lastKnownPosition');
+  const currentWeatherFromLocalStorage = localStorage.getItem('currentWeather');
+  if (
+    lastKnownPositionFromLocalStorage
+    && currentWeatherFromLocalStorage
+    && !store.state.currentWeather.dataLoadedFromAPI
+  ) {
+    store.commit(
+      mutationTypes.SAVE_CURRENT_POSITION_DATA,
+      JSON.parse(lastKnownPositionFromLocalStorage),
+    );
+    store.commit(mutationTypes.SAVE_WEATHER, JSON.parse(currentWeatherFromLocalStorage));
   }
   store.subscribe((mutation) => {
     if (!localStorage) return;

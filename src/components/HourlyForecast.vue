8045fe3e36689d8f47ddbdc6a8f83ee019e2225a
liubich <liubich.a@gmail.com>
2019-11-28 23:37:30 +0200

    Finished captions column

--- src/components/HourlyForecast.vue
+++ src/components/HourlyForecast.vue
@@ -1,23 +1,28 @@
 <template functional>
   <div class="hourly-forecast">
     <div class="hourly-forecast__column-captions column">
-      <div class="column__time">Час</div>
-      <div class="column__icon-caption"></div>
+      <div class="captions-column__time">Час</div>
+      <div class="captions-column__icon-caption">Опис</div>
+      <div class="captions-column__temperature">Температура</div>
+      <div class="captions-column__appearing-temperature">Відчувається як</div>
+      <div class="captions-column__pressure">Тиск</div>
     </div>
-    <div
-      v-for="(hourForecast, index) in props.weather.data"
-      :key="index"
-      class="hourly-forecast__column column"
-    >
-      <div class="column__time">{{ hourForecast.time }}</div>
-      <div class="column__icon icon" :data-tooltip="hourForecast.weatherDescription">
-        <img :src="hourForecast.icon" alt="weather icon" class="column__icon-image" />
+    <div class="hourly-forecast__data-columns">
+      <div
+        v-for="(hourForecast, index) in props.weather.data"
+        :key="index"
+        class="hourly-forecast__column column"
+      >
+        <div class="column__time">{{ hourForecast.time }}</div>
+        <div class="column__icon icon" :data-tooltip="hourForecast.weatherDescription">
+          <img :src="hourForecast.icon" alt="weather icon" class="column__icon-image" />
+        </div>
+        <div class="column__temperature">{{ hourForecast.temperature }}</div>
+        <div class="column__appearing-temperature">
+          {{ hourForecast.appearingTemperature }}
+        </div>
+        <div class="column__pressure">{{ hourForecast.pressure }}</div>
       </div>
-      <div class="column__temperature">{{ hourForecast.temperature }}</div>
-      <div class="column__appearing-temperature">
-        {{ hourForecast.appearingTemperature }}
-      </div>
-      <div class="column__pressure">{{ hourForecast.pressure }}</div>
     </div>
   </div>
 </template>
@@ -31,13 +36,35 @@ export default {
 
 <style lang="scss" scoped>
 .hourly-forecast {
+  display: flex;
+  padding: 5px;
+  border: thick double #32a1ce;
   width: 100%;
   max-width: 480px;
-  padding: 5px;
+}
+
+.hourly-forecast__data-columns {
   display: flex;
   align-items: flex-start;
-  border: thick double #32a1ce;
   overflow-x: scroll;
+}
+
+.captions-column {
+  &__icon-caption {
+    height: 36px;
+    padding: 2px 10px;
+    font: bold 12px/36px Roboto, sans-serif;
+    white-space: nowrap;
+  }
+
+  &__time,
+  &__pressure,
+  &__temperature,
+  &__appearing-temperature {
+    padding: 2px 0;
+    font: bold 12px Roboto, sans-serif;
+    white-space: nowrap;
+  }
 }
 
 .column {
@@ -87,10 +114,6 @@ export default {
       white-space: nowrap;
       box-shadow: 0 2px 1px #bcbcbc;
     }
-  }
-
-  &__icon-caption {
-    height: 36px;
   }
 }
 </style>

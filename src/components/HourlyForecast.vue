15fa4fde8ad9207cc473cfff5e25ca311df29ddd
liubich <liubich.a@gmail.com>
2019-11-28 23:37:29 +0200

    Add pressure. Add tooltip with weather description. Add column with captions

--- src/components/HourlyForecast.vue
+++ src/components/HourlyForecast.vue
@@ -1,18 +1,23 @@
 <template functional>
   <div class="hourly-forecast">
+    <div class="hourly-forecast__column-captions column">
+      <div class="column__time">Час</div>
+      <div class="column__icon-caption"></div>
+    </div>
     <div
       v-for="(hourForecast, index) in props.weather.data"
       :key="index"
       class="hourly-forecast__column column"
     >
       <div class="column__time">{{ hourForecast.time }}</div>
-      <div class="column__icon-wrapper">
-        <img :src="hourForecast.icon" alt="weather icon" class="column__icon" />
+      <div class="column__icon icon" :data-tooltip="hourForecast.weatherDescription">
+        <img :src="hourForecast.icon" alt="weather icon" class="column__icon-image" />
       </div>
       <div class="column__temperature">{{ hourForecast.temperature }}</div>
       <div class="column__appearing-temperature">
         {{ hourForecast.appearingTemperature }}
       </div>
+      <div class="column__pressure">{{ hourForecast.pressure }}</div>
     </div>
   </div>
 </template>
@@ -48,13 +53,44 @@ export default {
     padding: 2px 10px;
   }
 
-  &__temperature {
+  &__pressure,
+  &__temperature,
+  &__appearing-temperature {
     padding: 2px 10px;
+    font-family: Roboto, sans-serif;
+    font-size: 12px;
   }
 
-  &__icon {
+  &__icon-image {
     width: 32px;
     height: 32px;
+  }
+
+  .icon[data-tooltip] {
+    position: relative;
+
+    &:hover::after,
+    &:focus::after {
+      content: attr(data-tooltip);
+      position: absolute;
+      left: -50%;
+      top: 36px;
+      border: 1px solid #cecece;
+      border-radius: 5px;
+      background-color: white;
+      padding: 2px 5px;
+      font-family: Roboto, sans-serif;
+      font-size: 12px;
+      font-weight: bold;
+      z-index: 1;
+      opacity: 1;
+      white-space: nowrap;
+      box-shadow: 0 2px 1px #bcbcbc;
+    }
+  }
+
+  &__icon-caption {
+    height: 36px;
   }
 }
 </style>

249baf0d323c2017d034a64d435b8651b46e3aef
liubich <liubich.a@gmail.com>
2019-11-28 23:37:02 +0200

    Add component WeatherForecast

---
+++ src/components/WeatherForecast.vue
@@ -0,0 +1,22 @@
+<template functional>
+  <div class="weather-forecast"></div>
+</template>
+
+<script>
+export default {
+  name: 'WeatherForecast',
+  props: ['weather', 'currentPosition'],
+};
+</script>
+
+<style lang="scss" scoped>
+.weather-forecast {
+  width: 100%;
+  max-width: 480px;
+  padding: 5px;
+  display: flex;
+  align-items: flex-start;
+  flex-direction: column;
+  border: thick double #32a1ce;
+}
+</style>

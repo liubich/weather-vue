ce02a1aca47263922ad1ef090dfa15d347458e49
liubich <liubich.a@gmail.com>
2019-11-28 23:37:08 +0200

    Add temperature displaying

--- src/components/HourlyForecast.vue
+++ src/components/HourlyForecast.vue
@@ -1,13 +1,19 @@
 <template functional>
   <div class="hourly-forecast">
-    <div class="hourly-forecast__column column"></div>
+    <div
+      v-for="(hourForecast, index) in props.weather"
+      :key="index"
+      class="hourly-forecast__column column"
+    >
+      <div>{{ props.weather[index].temp }}</div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'HourlyForecast',
-  props: ['weather',],
+  props: ['weather'],
 };
 </script>
 
@@ -18,7 +24,11 @@ export default {
   padding: 5px;
   display: flex;
   align-items: flex-start;
-  // flex-direction: column;
   border: thick double #32a1ce;
+}
+
+.column {
+  display: flex;
+  flex-direction: column;
 }
 </style>

7b2c92489a0c461cc549d19d24c78d338b006b58
liubich <liubich.a@gmail.com>
2019-11-28 23:37:07 +0200

    Rename component to HourlyForecast

--- src/components/HourlyForecast.vue
+++ src/components/HourlyForecast.vue
@@ -1,22 +1,24 @@
 <template functional>
-  <div class="weather-forecast"></div>
+  <div class="hourly-forecast">
+    <div class="hourly-forecast__column column"></div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'WeatherForecast',
-  props: ['weather', 'currentPosition'],
+  name: 'HourlyForecast',
+  props: ['weather',],
 };
 </script>
 
 <style lang="scss" scoped>
-.weather-forecast {
+.hourly-forecast {
   width: 100%;
   max-width: 480px;
   padding: 5px;
   display: flex;
   align-items: flex-start;
-  flex-direction: column;
+  // flex-direction: column;
   border: thick double #32a1ce;
 }
 </style>

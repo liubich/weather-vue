c94abafe3bc3945ed6e22fcf764b7267b1253387
Liubich <liubich.a@gmail.com>
2020-01-03 14:06:41 +0200

    Move styles into separate css file

--- src/App.vue
+++ src/App.vue
@@ -38,20 +38,6 @@ export default {
 </script>
 
 <style>
-.theme-light {
-  --main-text-color: 'black';
-  --shadow-text-color: 'dimgray';
-  --main-back-color: 'white';
-  --shadow-back-color: '#f4f4f4';
-}
-
-.theme-dark {
-  --main-text-color: 'darkgreen';
-  --shadow-text-color: '#115011';
-  --main-back-color: '#222';
-  --shadow-back-color: 'black';
-}
-
 * {
   margin: 0;
   padding: 0;

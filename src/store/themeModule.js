684f90cf7f79cec72b0f4324df54494cfb4b9b86
liubich <liubich.a@gmail.com>
2020-04-19 20:16:09 +0300

    Add source to theme saving mutation

--- src/store/themeModule.js
+++ src/store/themeModule.js
@@ -2,14 +2,28 @@ import { SAVE_PREFERRED_THEME } from './mutationTypes';
 
 export default {
   state: {
-    preferredTheme: null,
+    osTheme: null,
+    localStorageTheme: null,
+    toggleTheme: null,
   },
   mutations: {
-    [SAVE_PREFERRED_THEME](state, preferredTheme) {
-      state.preferredTheme = preferredTheme;
+    [SAVE_PREFERRED_THEME](state, { theme, source }) {
+      switch (source) {
+        case 'os':
+          state.osTheme = theme;
+          break;
+        case 'localstorage':
+          state.localStorageTheme = theme;
+          break;
+        case 'toggle':
+          state.toggleTheme = theme;
+          break;
+        default:
+          break;
+      }
     },
   },
   getters: {
-    isDarkTheme: (state) => state.preferredTheme === 'dark',
+    isDarkTheme: (state) => state.osTheme === 'dark',
   },
 };
